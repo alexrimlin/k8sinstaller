1e219822abb91a1aeed7c19afd50cabc8b66f653
camelCat <63896744+camelCat@users.noreply.github.com>
2021-05-03 17:52:03 +0300

    Update glusterfs.sh

--- glusterfs.sh
+++ glusterfs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 
-apt install software-properties-common
-add-apt-repository ppa:gluster/glusterfs-7
+apt install software-properties-common -y
+add-apt-repository ppa:gluster/glusterfs-7 -y
 apt update
-apt install glusterfs-server
+apt install glusterfs-server -y
 systemctl start glusterd
